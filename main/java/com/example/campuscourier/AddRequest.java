7a4078bd9e76d82c97c3a175b482694ba92ef6e6
limjiahui3 <73053093+limjiahui3@users.noreply.github.com>
2024-03-27 19:12:30 +0800

    Add files via upload

---
+++ main/java/com/example/campuscourier/AddRequest.java
@@ -0,0 +1,218 @@
+package com.example.campuscourier;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.TimePicker;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+public class AddRequest extends AppCompatActivity {
+
+    TextInputEditText inputItem, inputDescription;
+    DatePicker datePicker;
+    TimePicker timePicker;
+    Spinner inputLocation, inputUrgency;
+    Button buttonAddImage, buttonRemoveImage, buttonPost;
+    ImageView imageView;
+    int SELECT_PICTURE = 200;
+    Uri selectedImageUri;
+    FirebaseAuth mAuth;
+    FirebaseFirestore db;
+    StorageReference storageRef;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_request);
+
+        mAuth = FirebaseAuth.getInstance();
+        db = FirebaseFirestore.getInstance();
+        storageRef = FirebaseStorage.getInstance().getReference();
+        String userId = Objects.requireNonNull(mAuth.getCurrentUser()).getUid();
+
+        inputItem = findViewById(R.id.inputItem);
+        inputDescription = findViewById(R.id.inputDescription);
+        buttonAddImage = findViewById(R.id.buttonAddImage);
+        buttonRemoveImage = findViewById(R.id.buttonRemoveImage);
+        imageView = findViewById(R.id.image);
+        datePicker = findViewById(R.id.date);
+        timePicker = findViewById(R.id.time);
+        timePicker.setIs24HourView(true);
+        inputLocation = findViewById(R.id.inputLocation);
+        inputUrgency = findViewById(R.id.inputUrgency);
+        buttonPost = findViewById(R.id.buttonPost);
+
+        buttonAddImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imageChooser();
+            }});
+
+        buttonRemoveImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (null != selectedImageUri) {
+                    imageView.setImageDrawable(null);
+                }
+            }
+        });
+
+        ArrayAdapter<CharSequence> adapterLocation = ArrayAdapter.createFromResource(this, R.array.locations, android.R.layout.simple_spinner_item);
+        adapterLocation.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        inputLocation.setAdapter(adapterLocation);
+
+        ArrayAdapter<CharSequence> adapterUrgency = ArrayAdapter.createFromResource(this, R.array.urgency, android.R.layout.simple_spinner_item);
+        adapterUrgency.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        inputUrgency.setAdapter(adapterUrgency);
+
+        buttonPost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String item;
+                String description;
+                String date;
+                String time;
+                String location;
+                String urgency;
+                item = String.valueOf(inputItem.getText());
+                description = String.valueOf(inputDescription.getText());
+                date = datePicker.getYear()+format(datePicker.getMonth())+format(datePicker.getDayOfMonth());
+                time = format(timePicker.getHour())+format(timePicker.getMinute());
+                location = String.valueOf(inputLocation.getSelectedItem());
+                urgency = String.valueOf(inputUrgency.getSelectedItem());
+
+                if (TextUtils.isEmpty(item)){
+                    Toast.makeText(getApplicationContext(), "Enter item", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                Map<String, Object> post = new HashMap<>();
+                post.put("Item", item);
+                if (!TextUtils.isEmpty(description)) {
+                    post.put("Description", description);
+                }
+                if (null != selectedImageUri) {
+                    // Get the data from an ImageView as bytes
+                    String imageUri = String.valueOf(selectedImageUri);
+                    StorageReference imageRef = storageRef.child(userId).child(imageUri);
+                    imageView.setDrawingCacheEnabled(true);
+                    imageView.buildDrawingCache();
+                    Bitmap bitmap = ((BitmapDrawable) imageView.getDrawable()).getBitmap();
+                    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                    bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+                    byte[] data = baos.toByteArray();
+                    imageRef.putBytes(data);
+                    String imageStorageUri = String.valueOf(imageRef);
+                    post.put("Image URI", imageStorageUri);
+                }
+                post.put("Expiry Date", date);
+                post.put("Expiry Time", time);
+                post.put("Location", location);
+                post.put("Urgency", urgency);
+                post.put("ID", userId);
+                db.collection("users").document(Objects.requireNonNull(mAuth.getCurrentUser()).getUid()).collection("posts")
+                        .add(post)
+                        .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                            @Override
+                            public void onSuccess(DocumentReference documentReference) {
+                                Log.d("NEW REQUEST", "request stored");
+                                Toast.makeText(getApplicationContext(), "Request created.", Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Log.w("NEW REQUEST", "request not stored", e);
+                                Toast.makeText(getApplicationContext(), "Request not created. Try again.", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                db.collection("posts").add(post)
+                        .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                            @Override
+                            public void onSuccess(DocumentReference documentReference) {
+                                Log.d("NEW REQUEST", "request stored");
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Log.w("NEW REQUEST", "request not stored", e);
+                            }
+                        });
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+    // this function is triggered when
+            // the Select Image Button is clicked
+    void imageChooser() {
+        // create an instance of the
+        // intent of the type image
+        Intent i = new Intent();
+        i.setType("image/*");
+        i.setAction(Intent.ACTION_GET_CONTENT);
+        // pass the constant to compare it
+        // with the returned requestCode
+        startActivityForResult(Intent.createChooser(i, "Select Picture"), SELECT_PICTURE);
+    }
+    // this function is triggered when user
+    // selects the image from the imageChooser
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            // compare the resultCode with the
+            // SELECT_PICTURE constant
+            if (requestCode == SELECT_PICTURE) {
+                // Get the url of the image from data
+                selectedImageUri = data.getData();
+                if (null != selectedImageUri) {
+                    Bitmap bitmap;
+                    try {
+                        bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), selectedImageUri);
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                    imageView.setImageBitmap(Bitmap.createScaledBitmap(bitmap, bitmap.getWidth()/2, bitmap.getHeight()/2, true));
+                }
+            }
+        }
+    }
+
+    public String format(int x){
+        if (String.valueOf(x).length()==1){
+            return "0"+x;
+        }
+        return String.valueOf(x);
+    }
+}
